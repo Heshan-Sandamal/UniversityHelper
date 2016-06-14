4138af92ffe53180fb0551ba9c95257c1bb2a6cd
Heshan-Sandamal <mhsandamal1@gmail.com>
2016-06-15 00:18:12 +0530

    update apartment, update landlorddetails, view apartment details

---
+++ src/com/universityHelper/controllers/UpdateApartment.java
@@ -0,0 +1,133 @@
+package com.universityHelper.controllers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.junit.Assert;
+
+import com.universityHelper.models.Apartment;
+import com.universityHelper.models.ApartmentKey;
+import com.universityHelper.models.LandLord;
+import com.universityHelper.models.University;
+import com.universityHelper.services.ApartmentServiceLocal;
+import com.universityHelper.services.LandLordServiceLocal;
+import com.universityHelper.services.UniversityServiceLocal;
+
+import sun.misc.FloatingDecimal;
+
+/**
+ * Servlet implementation class LLsignUp
+ */
+@WebServlet("/UpdateApartment")
+public class UpdateApartment extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	// Inject ApartmentService to servlet
+	@EJB
+	ApartmentServiceLocal apartmentService;
+
+	@EJB
+	UniversityServiceLocal universityService;
+
+	public UpdateApartment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		String apartmentId=request.getParameter("ApartmentId");
+		
+		Apartment apartment=apartmentService.getApartmentDetailsForUpdate(apartmentId);
+		
+		// send AddApartment interface to view layer
+		RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/UpdateApartment.jsp");
+
+		request.setAttribute("UniversityList", universityService.getAllUniversities());
+		request.setAttribute("apartment", apartment);
+
+		view.forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Object apartmentOwnerId = request.getSession().getAttribute("ApartmentOwnerId");
+
+		if (apartmentOwnerId != null) {
+			// get details from front end
+			
+			String apartmentKey=request.getParameter("apartmentKey");
+			String name = request.getParameter("name");
+			String address = request.getParameter("address");
+			double lattitude = FloatingDecimal.parseDouble(request.getParameter("lattitude"));
+			double longitude = FloatingDecimal.parseDouble(request.getParameter("longitude"));
+			double payment = FloatingDecimal.parseDouble(request.getParameter("payment"));
+			int avilablePlaces = Integer.parseInt(request.getParameter("availability"));
+			String university = request.getParameter("university");
+			String studentSex = request.getParameter("studentSex");
+			String[] universityList = request.getParameterValues("univesities");
+			
+			
+			System.out.println(studentSex);
+			
+			// create Apartment object using submitted details
+			Apartment ap = new Apartment();
+
+			// set longitude and lattitude
+			ap.setLattitide(lattitude);
+			ap.setLongitude(longitude);
+			ap.setApartmentKey(apartmentKey);
+
+			ap.setName(name); // set Apartment Name
+			ap.setAvilablePlaces(avilablePlaces); // set availablePlaces
+			ap.setAddress(address); // set address
+			ap.setPayment(payment); // set payment
+			ap.setRate(0.0);
+			ap.setStudentSex(studentSex);
+
+			// send apartment object to apartment service bean to persists
+			boolean aptStatus = apartmentService.updateApartment(ap, universityList);
+			// Assert.assertTrue(aptStatus);
+
+			// ap.getUniversity().add(arg0)
+
+			if (aptStatus) {
+				response.sendRedirect("LLHome");
+			} else {
+				response.getWriter().println("Unable to update details");
+			}
+		}
+
+	}
+
+}
---
+++ src/com/universityHelper/controllers/UpdateLandLordDetails.java
@@ -0,0 +1,104 @@
+package com.universityHelper.controllers;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.universityHelper.models.LandLord;
+import com.universityHelper.models.LandLordProfile;
+import com.universityHelper.models.Student;
+import com.universityHelper.models.StudentProfile;
+import com.universityHelper.other.Encrypt;
+import com.universityHelper.services.LandLordService;
+import com.universityHelper.services.LandLordServiceLocal;
+import com.universityHelper.services.StudentServiceLocal;
+
+/**
+ * Servlet implementation class StudentSignUp
+ */
+@WebServlet("/UpdateLandLordDetails")
+public class UpdateLandLordDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	LandLordServiceLocal landLordService;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UpdateLandLordDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		LandLord landLord = landLordService
+				.getLandLord(request.getSession().getAttribute("ApartmentOwnerId").toString());
+		RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/UpdateLandLordDetails.jsp");
+		request.setAttribute("landLord", landLord);
+		view.forward(request, response);
+
+		// response.getWriter().append("Served at:
+		// ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		LandLordProfile llp = new LandLordProfile();
+		llp.setUserName(request.getParameter("userName"));
+		llp.setPassword(request.getParameter("password"));
+
+		LandLord landLord = new LandLord();
+		landLord.setLandLordId(request.getSession().getAttribute("ApartmentOwnerId").toString());
+		landLord.setFirstName(request.getParameter("firstName"));
+		landLord.setLastName(request.getParameter("lastName"));
+		landLord.setAddress(request.getParameter("address"));
+		landLord.setEmail(request.getParameter("email"));
+
+		Set<String> contactNoList = new HashSet<String>();
+
+		String contactNo1 = request.getParameter("contactNo1");
+		String contactNo2 = request.getParameter("contactNo2");
+
+		if (!contactNo1.isEmpty()) {
+			contactNoList.add(contactNo1);
+		}
+
+		if (!contactNo2.isEmpty()) {
+			contactNoList.add(contactNo2);
+		}
+
+		landLord.setContactNoList(contactNoList);
+
+		llp.setLandLord(landLord);
+		landLord.setLandLordProfile(llp);
+
+		landLordService.updateLandLord(landLord, llp);
+
+		//response.getWriter().write("sighUp");
+	}
+}
